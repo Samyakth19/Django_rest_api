cc81a20057b13456317be83b17e8469b0a15fc80
SAMYAKTH KUMAR <samyakthkumar19@gmail.com>
2022-12-04 22:12:08 +0530

    added h1 tag

--- companyapi/companyapi/views.py
+++ companyapi/companyapi/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 def home_page(request):
     print("Home page requested")
-    return HttpResponse("This is Home page")
+    return HttpResponse("<h1>This is Home page<h1>")
