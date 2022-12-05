431e641485044fef9881d8acddeca863b309309f
SAMYAKTH KUMAR <samyakthkumar19@gmail.com>
2022-12-05 17:29:55 +0530

    intial commit

--- companyapi/api/serializers.py
+++ companyapi/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from api.models import Company 
 # create serializers here
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    Company_id = serializers.ReadOnlyField()
     class Meta:
         model = Company
         fields = "__all__"
